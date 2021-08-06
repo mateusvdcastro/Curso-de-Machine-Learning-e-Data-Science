660c90c47a41318af584048a91abe7005c4eacb1
Mateus Vespasiano de Castro <mateusvc1124@gmail.com>
2021-08-05 23:02:17 -0300

    VAE Update

--- Pycharm Machine Learning/Codificador Automático Variacional (VAE)/Generating Sound Digits with a Variational AutoEncoder/soundgeneration.py
+++ Pycharm Machine Learning/Codificador Automático Variacional (VAE)/Generating Sound Digits with a Variational AutoEncoder/soundgeneration.py
@@ -2,6 +2,7 @@ import librosa
 
 from preprocess import MinMaxNormaliser
 
+
 class SoundGenerator:
     """SoundGenerator is responsible for generating audios
     from spectrograms."""
@@ -22,7 +23,12 @@ class SoundGenerator:
             # reshape the log spectrogram
             log_spectrogram = spectrogram[:, :, 0]  # we are coping the first and secound dimension, and discarding the third
             # apply denormalization
-            denorm_log_spec =
+            denorm_log_spec = self._min_max_normaliser.denormalise(
+                log_spectrogram, min_max_value["min"], min_max_value["max"])
             # log spectrogram -> spectrogram
+            spec = librosa.db_to_amplitude(denorm_log_spec)
             # apply Griffin-Lim
+            signal = librosa.istft(spec, hop_length=self.hop_length)
             # append signal to "signals"
+            signals.append(signal)
+        return signals
