01ad44d1c7f50b9dbc3e064e272745d9ae61bc11
Mateus Vespasiano de Castro <mateusvc1124@gmail.com>
2021-08-10 17:51:12 -0300

    MLP Neural Network

--- Pycharm Machine Learning/Deep Learning (for Audio) with Python/ Implementing backpropagation and gradient descent from scratch/mlp_train.py
+++ Pycharm Machine Learning/Deep Learning (for Audio) with Python/ Implementing backpropagation and gradient descent from scratch/mlp_train.py
@@ -1,4 +1,5 @@
 import numpy as np
+from random import random
 
 # save activations and derivates
 # implement backpropagation
@@ -8,15 +9,15 @@ import numpy as np
 # make some predictions
 
 
-class MLP:
-    def __init__(self, num_inputs=3, num_hidden=[3, 5], num_outputs=2):
+class MLP(object):
+    def __init__(self, num_inputs=3, hidden_layers=[3, 5], num_outputs=2):
         # num_hidden=[3, 5] -> this mean that we have 2 hidden layers, the first
         # contain 3 neurons and the secound contain 5 neurons;
         self.num_inputs = num_inputs
-        self.num_hidden = num_hidden
+        self.hidden_layers = hidden_layers
         self.num_outputs = num_outputs
 
-        layers = [num_inputs] + num_hidden + [num_outputs]
+        layers = [num_inputs] + hidden_layers + [num_outputs]
 
         # initiate random weights
         weights = []
@@ -60,7 +61,7 @@ class MLP:
 
         return activations
 
-    def back_propagate(self, error, verbose=False):
+    def back_propagate(self, error):
         # Note: error == (y - a[i+1])
 
         # dE/dW_i = (y - a[i+1])s'(h_[i+1]))a_i        y  is the real value and a the value predicted
@@ -77,9 +78,38 @@ class MLP:
             current_activations_reshaped = current_activations.reshape(current_activations.shape[0], -1)
             self.derivatives[i] = np.dot(current_activations_reshaped, delta_reshaped)
             error = np.dot(delta, self.weights[i].T)  # error == (y - a[i+1])s'(h_[i+1]))W_i
-            if verbose:
-                print(f"Derivatives for W{i}: {self.derivatives}")
-        return error
+
+    def gradient_descent(self, learning_rate=1):
+        for i in range(len(self.weights)):
+            weights = self.weights[i]
+            derivatives = self.derivatives[i]
+            weights += derivatives * learning_rate
+
+    def train(self, inputs, targets, epochs, learning_rate):  # epochs -> how many times we wanted feed the neural network
+        for i in range(epochs):
+            sum_error = 0
+            for j, input in enumerate(inputs):
+                target = targets[j]
+
+                # forward propagation
+                output = self.forward_propagate(input)
+
+                # calculate error
+                error = target - output
+
+                # back propagation
+                self.back_propagate(error)
+
+                # apply gradient discent
+                self.gradient_descent(learning_rate)
+
+                sum_error += self._mse(target, output)
+
+            # report error for each epoch
+            print(f"Error: {sum_error/ len(inputs)} at epoch {i+1}")
+
+    def _mse(self, target, output):
+        return np.average((target-output)**2)
 
     def _sigmoid_derivative(self, x):
         return x * (1.0 - x)
@@ -89,18 +119,28 @@ class MLP:
 
 
 if __name__ == "__main__":
+
+    # create a dataset to train a network for the sum operation
+    inputs = np.array([[random() / 2 for _ in range(2)] for _ in range(1000)])  # array([0.1, 0.2], [0.3, 0.4])
+    targets = np.array([[i[0] + i[1]] for i in inputs])  # array([0.3], [0.7])
+
     # create an MLP
     mlp = MLP(2, [5], 1)  # 2 inputs, one hidden layer with 5 neurons, 1 output
 
+    # train our mlp
+    mlp.train(inputs, targets, 50, 0.1)
+
     # create dummy data
-    inputs = np.array([0.1, 0.2])  # the neural network must sum inputs and arrive in target
-    target = np.array([0.3])
+    input = np.array([0.3, 0.1])
+    target = np.array([0.4])
 
-    # forward propagation
-    output = mlp.forward_propagate(inputs)
+    output = mlp.forward_propagate(input)
+    print()
+    print()
+    print(f"Our network believes that {input[0]} + {input[1]} is equal to {output[0]}.")
 
-    # calculate error
-    error = target - output
 
-    # back propagation
-    mlp.back_propagate(error, verbose=True)
+    '''# create dummy data
+        inputs = np.array([0.1, 0.2])  # the neural network must sum inputs and arrive in target
+        target = np.array([0.3])'''
+
